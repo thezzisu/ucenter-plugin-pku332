b4aa56f28706710756f9dc34f4d6ad492239f831
thezzisu <thezzisu@gmail.com>
2022-11-27 17:31:01 +0800

    chore: update

--- ui/src/IndexPage.vue
+++ ui/src/IndexPage.vue
@@ -4,25 +4,13 @@
       <div>
         <img class="p-logo" src="https://asset.zisu.dev/img/332_512x512.png" />
       </div>
-      <div class="font-bold text-5xl pt-8">45乙332寝室</div>
+      <div class="font-bold text-5xl pt-8">摆带小助手</div>
       <div v-if="isLoggedIn" class="text-2xl py-2">
         欢迎您的到来，尊敬的
         <b>{{ userInfo.attributes.nickname ?? userInfo.attributes.name }}</b>
         。
       </div>
       <div v-else>请登录以使用更多功能。</div>
-      <div>
-        <iframe
-          class="p-desc-video"
-          src="//player.bilibili.com/player.html?aid=553982621&bvid=BV1Dv4y1P72j&cid=716733007&page=1"
-          scrolling="no"
-          border="0"
-          frameborder="no"
-          framespacing="0"
-          allowfullscreen="true"
-        >
-        </iframe>
-      </div>
     </div>
   </div>
 </template>
